b5db861035a1a1ee96e6d30f3ca54c48940c8027
RubelMozumder <rubelmozumder@outlook.com>
2021-07-06 14:11:22 +0200

    starting the IngestorInterfave

---
+++ QuoteEngine/IngestorInterface.py
@@ -0,0 +1,23 @@
+"""Module IngestorInterface.
+Module encapsulates IngestorInterface class with all the generic 
+'abstractmethods' and 'classmethods'.
+"""
+
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class IngestorInterface(ABC):
+    """Abstractclass IngestoInterface."""
+
+    allowed_file_extention = []
+    
+    @abstractclass
+    def parse(cls, path: str) -> List[QuoteModel]:
+        "parse function should be implemented at the stratgy class"
+        pass
+
+    @classmethod
+    def can_ingest(cls, path: str) -> boolean:
+        "can_ingest class method will check for correct file extension"
--- QuoteEngine/QuoteModel.py
+++ QuoteEngine/QuoteModel.py
@@ -1,8 +1,9 @@
-"""Module for encapsulating Quote and Author"""
+"""Module for encapsulating Quote and Author."""
 
 
 class QuoteModel:
     """Class for collect the Quote and Author.
+
     Arguments:
         quote (str) -> Quote body.
         author (str) -> Name of Author of the citation
@@ -13,7 +14,22 @@ class QuoteModel:
     Usages:
         QuoteModel(quote, author) -> Instantiate QuoteModel object
     or,
-        QuoteModel.quote_parser(quote, author) -> 
-                                    Instantiate QuoteModel Obhect
+        QuoteModel.quote_parser(quote, author) ->
+                            Instantiate QuoteModel Object
     """
 
+    def __init__(self, quote: str, author: str) -> None:
+        """Parameters: quote(str), author(str).
+
+        Return: its own instantiated object
+        """
+        self.quote = quote
+        self.author = author
+
+    @classmethod
+    def quote_parser(cls, quote: str, author: str) -> object:
+        """parameter: quote(str), author(str).
+
+        Return: instantiated QuoteModel object
+        """
+        return cls(quote, author)
