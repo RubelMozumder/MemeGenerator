5417753a496b2cfad5838afe4e7b9a69c2726092
RubelMozumder <rubelmozumder@outlook.com>
2021-07-06 22:41:37 +0200

    continue

--- QuoteEngine/IngestorInterface.py
+++ QuoteEngine/IngestorInterface.py
@@ -1,26 +1,27 @@
 """Module IngestorInterface.
-Module encapsulates IngestorInterface class with all the generic 
+
+Module encapsulates IngestorInterface class with all the generic
 'abstractmethods' and 'classmethods'.
 """
 
 
 from abc import ABC, abstractmethod
 from typing import List
+from .QuoteModel import QuoteModel
 
 
 class IngestorInterface(ABC):
     """Abstractclass IngestoInterface."""
 
-    allowed_file_extention = []
-    
-    @abstractclass
+    allowed_file_extention: List[str] = []
+
+    @abstractmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        "parse function should be implemented at the stratgy class"
+        """Parse function should be implemented at the stratgy class."""
         pass
 
     @classmethod
-    def can_ingest(cls, path: str) -> boolean:
-        "can_ingest classmethod will check for correct file extension"
+    def can_ingest(cls, path: str) -> bool:
+        """Can_ingest classmethod will check for correct file extension."""
         extention = path.split('.')[-1]
-        return extention in allowed_file_extention
-            
+        return extention in cls.allowed_file_extention
