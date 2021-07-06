87117738fa62b237bb1be093ba6f63db7090526d
RubelMozumder <rubelmozumder@outlook.com>
2021-07-06 11:33:02 +0200

    : :q

---
+++ QuoteEngine/QuoteModel.py
@@ -0,0 +1,19 @@
+"""Module for encapsulating Quote and Author"""
+
+
+class QuoteModel:
+    """Class for collect the Quote and Author.
+    Arguments:
+        quote (str) -> Quote body.
+        author (str) -> Name of Author of the citation
+
+    Return:
+        QuoteModel object.
+
+    Usages:
+        QuoteModel(quote, author) -> Instantiate QuoteModel object
+    or,
+        QuoteModel.quote_parser(quote, author) -> 
+                                    Instantiate QuoteModel Obhect
+    """
+
